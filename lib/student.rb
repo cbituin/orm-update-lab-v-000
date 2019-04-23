8f882b7f6948d7516ef6dcef85defbee19994e80
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-23 12:33:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize
+  attr_accessor :id, :name, :grade
 
+  def initialize(id=nil, name, grade)
+    @id = id 
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
